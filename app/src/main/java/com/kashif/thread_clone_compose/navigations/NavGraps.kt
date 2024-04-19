ff5d9770e4a27894a5ecb62debc4c5acd29ab462
kashif ansari <mhd.kashif1@gmail.com>
2024-04-19 07:19:17 +0530

    creating project

---
+++ app/src/main/java/com/kashif/thread_clone_compose/navigations/NavGraps.kt
@@ -0,0 +1,12 @@
+package com.kashif.thread_clone_compose.navigations
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Created by Mohammad Kashif Ansari on 19,April,2024
+ */
+
+@Composable
+fun NavGraph(){
+
+}
